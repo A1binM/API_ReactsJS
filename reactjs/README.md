8148f0aa62be882fc6e164d02549de99c262d4ff
A1binM <92087018+A1binM@users.noreply.github.com>
2023-05-23 21:06:21 +0200

    Add files via upload

---
+++ reactjs/README.md
@@ -0,0 +1,5 @@
+Din Måltid är en sida som ger dig förslag på måltider med bilder och namn samt länken till receptet. 
+
+Din Måltid ska kunna ge användaren möjligheten att kunna se namnen, bilderna och recepten på måltiderna som sidan rekommenderar, sidan ska även uppdatera de 10 olika måltiderna vid uppdatering (refresh)
+
+API jag har använt ger 1 slumpmässig måltid, om jag hade velat hade jag kunnat med hjälp av APIn gjort att enbart en sort eller kategori av mat kommer upp. Jag hade även kunna haft med ett youtube klipp på hur man gör måltiderna.
---
+++ reactjs/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import React, { useState, useEffect } from "react";
+
+function App() {
+  //meals är arrayen som kommer med API
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      //gör en kopia av state som sedan skickar tillbaka den
+      //delar upp meals arrayn i delar, de delarna blir lika med NewMeals
+      var NewMeals = meals.slice();
+
+      //ger ett värde till antalet målrätter / recept, används så att den slutar ge ut recept / måltider efter 10
+      var RecipeAmount = 0;
+      while(RecipeAmount < 10) {
+        const Answer = await fetch(
+          "https://www.themealdb.com/api/json/v1/1/random.php"
+        );
+        const DataBase = await Answer.json();
+        console.log(DataBase.meals[0].strCategory);
+
+        //fixar till så att kopian av state ligger i rätt plats i arrayn
+        //lägger till +1 till variablen så att loopen slutar vid 10
+        NewMeals[RecipeAmount] = DataBase.meals[0];
+        RecipeAmount++; 
+      }
+      //skickar iväg state kopian 
+      setMeals(NewMeals);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    //olika div för styling
+    <div class="header">
+      <div class="test">
+        <h1 class="header">Hej och välkommen till Din Måltid </h1>
+        <p>
+          Här får du 10 autogenerade maträtter med bilder och länkar till recept
+        </p>
+      </div>
+      {/*går igenom varje värde i state:en*/}
+      {meals.map((meal, index) => (
+        <div class="item">
+          {/*variablerna inom {} ger ett värde såsom string eller bild, det läggs till i sidan*/}
+          <h2>{meal.Number}</h2>
+          <h3>{meal.strMeal}.</h3>
+          <p> Kategori: {meal.strCategory}</p>
+          <p>
+            Hela receptet :{" "}
+            <a href={meal.strSource}>
+              {meal.strMeal}
+            </a>
+          </p>
+          <img
+            src={meal.strMealThumb}
+            alt={meal.Number}
+            width="10%"
+            height="10%"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
